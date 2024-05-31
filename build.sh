b5831677e4b66164fca29a48638c224d41dceede
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-05-31 07:58:21 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,5 @@ docker stop prometheus-vastai-exporter-1
 docker rm prometheus-vastai-exporter-1
 docker build -t alkorolyov/vastai-exporter .
 docker push alkorolyov/vastai-exporter
-cd /home/vast/prometheus/
 docker-compose up -d
-cd /home/vast/prometheus-vastai
 
